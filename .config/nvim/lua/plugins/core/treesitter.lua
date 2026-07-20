d52875cde66afcb77be3a45f9553a2ac7bf2b95d
Scott Richardson <scottjr632@gmail.com>
2026-07-20 04:23:03 +0000

    fix nvim for real this time

--- .config/nvim/lua/plugins/core/treesitter.lua
+++ .config/nvim/lua/plugins/core/treesitter.lua
@@ -1,16 +1,28 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    local filetypes = {
+      "c",
+      "go",
+      "html",
+      "javascript",
+      "lua",
+      "python",
+      "query",
+      "typescript",
+      "vim",
+      "vimdoc",
+    }
 
-      configs.setup({
-          -- Neovim 0.12 ships a Lua parser. Keeping the older plugin parser
-          -- ahead of it causes its bundled Lua highlight query to fail.
-          ensure_installed = { "c", "vim", "vimdoc", "query", "javascript", "typescript", "python", "go", "html" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
- } 
+    -- The current nvim-treesitter API delegates highlighting to Neovim.
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = filetypes,
+      callback = function()
+        vim.treesitter.start()
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
+}
